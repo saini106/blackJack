4bda62e728845eeaf5e4e2127b9f9dcf74ddc820
Harman Saini <Home1@DESKTOP-TAAIR14>
2020-06-25 21:30:09 -0400

    Project 1

--- src/main/java/blackjack/Dealer.java
+++ src/main/java/blackjack/Dealer.java
@@ -3,12 +3,14 @@ package blackjack;
 public class Dealer extends Player 
 {
     private Card revealedCard;
+    
     public Dealer (Card c1, Card c2) {
         super(c1, c2);
     } 
     public Dealer () {
         super ();
     }
+    
 
    public Card getRevealCard () {return this.revealedCard;}
 }
